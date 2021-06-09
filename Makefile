011c5c33012c4ff4263e8f3595663c3f6e33b9a6
StoneCookie <pravsha63@gmail.com>
2021-06-09 19:41:58 +0300

    HW_6

---
+++ Makefile
@@ -0,0 +1,48 @@
+LOCALHOST_PROJECT_DIR := $(shell pwd "-"W)
+
+# IMPORT CONFIG WITH ENVS. You can change the default config with `make cnf="config_special.env" up-dev`
+cnf ?= $(LOCALHOST_PROJECT_DIR)/deploy/config.env
+include $(cnf)
+
+export $(shell sed 's/=.*//' $(cnf))
+
+.DEFAULT_GOAL := help
+# This will output the help for each task
+# thanks to https://marmelab.com/blog/2016/02/29/auto-documented-makefile.html
+help:## This is help.
+	@awk 'BEGIN {FS = ":.*?## "} /^[a-zA-Z_-]+:.*?## / {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}' $(MAKEFILE_LIST)
+
+.PHONY: help
+
+## Docker compose shortcuts
+up-dev stop-dev kill-dev build-dev: COMPOSE_FILE=./docker-compose.yml
+up-dev: ## Up current containers for dev
+	docker-compose -f $(COMPOSE_FILE) up -d
+
+.PHONY: up-dev
+
+stop-dev: ## Stop current containers for dev
+	docker-compose -f $(COMPOSE_FILE) stop
+
+.PHONY: stop-dev
+
+kill-dev: ## Kill current containers for dev
+	docker-compose -f $(COMPOSE_FILE) rm -s -f
+
+.PHONY: kill-dev
+
+build-dev: ## Save current containers for dev
+	docker-compose -f $(COMPOSE_FILE) build
+
+.PHONY: save-dev
+
+ifeq (run-dev, $(firstword $(MAKECMDGOALS)))
+  # use the rest as arguments for "run"
+  RUN_ARGS := $(wordlist 2,$(words $(MAKECMDGOALS)),$(MAKECMDGOALS))
+  # ...and turn them into do-nothing targets
+  $(eval $(RUN_ARGS):;@:)
+endif
+
+run-dev:
+	winpty docker run --name test -v /$(LOCALHOST_PROJECT_DIR):/app --interactive --tty composer $(RUN_ARGS)
+	winpty docker rm test
---
+++ src/Controller/TestController.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/test")
+ */
+class TestController
+{
+    /**
+     * @Route(path="/", methods={"GET"})
+     */
+    public function index()
+    {
+        return new Response(
+            json_encode(
+                [
+                    'some-value'=> 123,
+                    'some-value-string'=> 'some string'
+                ]
+            ),
+            Response::HTTP_OK,
+            [
+                'Content-type'=> 'application/json'
+            ]
+        );
+    }
+
+    /**
+     * @Route(path="/users", methods={"GET"})
+     */
+    public function users()
+    {
+        return new Response(
+            content: json_encode(
+                [
+                    new class ('Innokentiy', 21) {
+                        public $name;
+                        public $age;
+
+                        public function __construct($name, $age)
+                        {
+                            $this->name = $name;
+                            $this->age = $age;
+                        }
+                    },
+                    new class ('Fedor', 17) {
+                        public $name;
+                        public $age;
+
+                        public function __construct($name, $age)
+                        {
+                            $this->name = $name;
+                            $this->age = $age;
+                        }
+                    }
+                ]
+            ),
+            status: Response::HTTP_OK,
+            headers: [
+                'Content-type'=> 'application/json'
+            ]
+        );
+    }
+
+}
